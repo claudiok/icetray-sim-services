6729507fc38588b0e47637a655c83f56d724a116
Alex Olivas <olivas@icecube.umd.edu>
2006-11-22 19:21:29 +0000

    moving twr stuff from the branch to the trunk
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/sim-services/trunk@25525 16731396-06f5-0310-8873-f7f720988828

---
+++ public/sim-services/sim-source/I3TWRParamFiller.h
@@ -0,0 +1,43 @@
+#ifndef TWRPARAMFILLER_H
+#define TWRPARAMFILLER_H
+/**
+ * class: I3TWRParamFiller
+ *
+ * Version $Id: $
+ *
+ * date: $Date: $
+ *
+ * @author wikstrom
+ *
+ * (c) 2006 IceCube Collaboration
+ */
+
+#include "icetray/OMKey.h"
+#include "dataclasses/calibration/I3MCTWRParams.h"
+
+typedef std::map<OMKey,I3MCTWRParams> I3MCTWRParamsMap;
+I3_POINTER_TYPEDEFS(I3MCTWRParamsMap);
+
+namespace I3TWRParamFiller 
+{
+
+  bool IsOptical(int);
+  void FillElecConstants(std::string,std::string,I3MCTWRParamsMapPtr);
+  void FillStopDelay(std::string,I3MCTWRParamsMapPtr);
+  void FillDMADDThreshold(std::string,I3MCTWRParamsMapPtr);
+  void FillCableDelay(std::string,std::string,I3MCTWRParamsMapPtr);
+  void FillRelativeSensitivity(std::string,I3MCTWRParamsMapPtr);
+
+};
+
+inline bool I3TWRParamFiller::IsOptical(int wf_type){
+  //these waveform types correspond to optical channels
+  return ((wf_type==8) || 
+	  (wf_type==10) || 
+	  (wf_type==12) || 
+	  (wf_type==13) );
+}
+
+#endif
+
+
