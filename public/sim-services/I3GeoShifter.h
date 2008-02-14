a262e6eb9b660043a4d1917714cfb08c52b8c573
Alex Olivas <olivas@icecube.umd.edu>
2008-02-14 20:08:52 +0000

    improved comments in I3GeoShifter
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/sim-services/trunk@42229 16731396-06f5-0310-8873-f7f720988828

--- public/sim-services/I3GeoShifter.h
+++ public/sim-services/I3GeoShifter.h
@@ -27,13 +27,37 @@ namespace I3GeoShifter{
    * Calculates the center of the detector in X-Y.
    * Returns a std::pair<double,double> where first
    * is x and second is y.
+   * You can specify the detector by passing the corresponding
+   * I3OMGeo::OMType.  The default is I3OMGeo::IceCube, but other
+   * options include I3OMGeo::IceTop and I3OMGeo::AMANDA.
+   * Currently you can only specify one and only one detector.
    */
   std::pair<double,double> 
     DetectorCenter(I3FramePtr frame, 
 		    I3OMGeo::OMType omType = I3OMGeo::IceCube);
 
+  /**
+   * Shifts every particle position in the tree by the given value.
+   * The 'first' element in the pair represents the shift in x.
+   * The 'second' element in the pair represents the shift in y.
+   * So for : 
+   *   std::pair<doubl,double> shift(10.*I3Units::m, 20.*I3Units::m)
+   *   I3GeoShifter::ShiftTree(shift,mctree);
+   * The new particles will have positions:
+   *   x_prime = x_0 + shift.first
+   *   y_prime = y_0 + shift.second
+   */
   void ShiftTree(const std::pair<double,double>&, I3MCTreePtr);
 
+
+  /**
+   * Provided for convenience for generators.  This shifts
+   * all of the particles in the mctree to the center of the
+   * 'standard' geometry in the frame.  You can specify the 
+   * subdetector by passing the omType.  The default is I3OMGeo::IceCube, 
+   * but other options include I3OMGeo::IceTop and I3OMGeo::AMANDA.
+   * Currently you can only specify one and only one detector.
+   */
   void ShiftTree(I3FramePtr frame, I3MCTreePtr, 
 		 I3OMGeo::OMType omType = I3OMGeo::IceCube);
 					   
