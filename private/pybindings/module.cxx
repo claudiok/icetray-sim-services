cea575b821dfa22b747a32f48c59988a5bda9964
Henrike Wissing <wissing@icecube.wisc.edu>
2009-08-07 21:21:41 +0000

    added pybindigs
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/sim-services/trunk@57261 16731396-06f5-0310-8873-f7f720988828

---
+++ private/pybindings/module.cxx
@@ -0,0 +1,30 @@
+//
+//   Copyright (c) 2009
+//   the icecube collaboration
+//   $Id$
+//                                                                                                       
+//   @version $Revision$                                                                                                    
+//   @author Henrike Wissing 
+//
+
+#include <icetray/I3FrameObject.h>
+#include <icetray/load_project.h>
+
+using namespace boost::python;
+namespace bp = boost::python;
+#include <sim-services/I3GeoShifter.h>
+
+void
+shiftTreeToCenter(I3FramePtr frame , I3MCTreePtr tree, 
+		  I3OMGeo::OMType omType = I3OMGeo::IceCube)
+{
+  I3GeoShifter::ShiftTree(frame, tree, omType);
+}
+
+
+BOOST_PYTHON_MODULE(sim_services)
+{
+  load_project("libsim-services", false);
+  def("shiftTreeToCenter", &shiftTreeToCenter);
+}
+
