b5cf8ed1f9fac82e0d93c739f493ce7838267b14
Alex Olivas <olivas@icecube.umd.edu>
2009-10-21 21:47:33 +0000

    fixed test module.  member variable typo.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/sim-services/trunk@59087 16731396-06f5-0310-8873-f7f720988828

--- private/test/I3SimSourceTestModule.h
+++ private/test/I3SimSourceTestModule.h
@@ -180,7 +180,7 @@ class I3SimSourceTestModule : public I3Module
   double cal_mpe_disc_thresh_slope_;
 
   double cal_pmt_disc_thresh_slope_;
-  double cal_pmt_disc_thresh_intercept_;
+  double cal_pmt_disc_thresh_int_;
 
   double cal_inice_relative_efficiencies_;
   double cal_deepcore_relative_efficiencies_;
