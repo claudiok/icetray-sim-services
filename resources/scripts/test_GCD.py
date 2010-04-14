8c27e98358bbb30d8f25ed04f14106d328f62fb3
Alex Olivas <olivas@icecube.umd.edu>
2010-04-14 21:39:10 +0000

    updated script
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/sim-services/trunk@63278 16731396-06f5-0310-8873-f7f720988828

--- resources/scripts/test_GCD.py
+++ resources/scripts/test_GCD.py
@@ -36,8 +36,8 @@ dom_geo = geometry.omgeo
 dom_cal = calibration.domCal
 dom_status = status.domStatus
 
-strings_to_check = range(1,87)
-for s in strings_to_check:
+geo_strings_to_check = range(1,87)
+for s in geo_strings_to_check:
 	found = False
 	for e,p in dom_geo:
 		if e.GetString() == s :
@@ -45,16 +45,40 @@ for s in strings_to_check:
 	if not found:
 		print "string %d does not exist in the geometry" % s
 	
+c_and_d_strings_to_check = [ 
+	75, 76, 77, 78, 68, 69, 70, 71, 72, 73,
+	74, 60, 61, 62, 63, 64, 65, 66, 67, 52,
+	53, 54, 55, 56, 57, 58, 59, 44, 45, 46,
+	47, 48, 49, 50, 36, 83, 37, 38, 39, 40,
+	26, 27, 28, 29, 30, 17, 18, 19, 20, 21,
+	10, 11, 12, 13,  2,  3,  4,  5,  6
+	]
+
+for s in c_and_d_strings_to_check :
+	for omkey in [icetray.OMKey(s,om) for om in range(61)] :
+		found_cal = False
+		if omkey in dom_cal :
+			found_cal = True
+		found_stat = False
+		if omkey in dom_status :
+			found_stat = True
+		if found_cal and found_stat : continue
+	if not found_cal : print 'string %s is missing from the calibration' % s
+	if not found_stat : print 'string %s is missing from the detector status' % s
 
 for e,p in dom_geo:
 
 	if e in badOMs and e in dom_cal and e in dom_status:
 		print "DOM %s is in the bad DOM list but contains status and calibration records" % str(e)
 	
-	if e not in badOMs and e in dom_cal and e in dom_status:
+	if e not in badOMs and e in dom_cal and e in dom_status:				
 		cal_this_om = dom_cal[e]
 		status_this_om = dom_status[e]
+
+		if cal_this_om.DOMCalVersion != "7.3.3" :
+			print '  %s  %s' % (str(e), cal_this_om.DOMCalVersion)
+		
 		threshold = dataclasses.SPEPMTThreshold(status_this_om,
 							cal_this_om) / I3Units.mV
 		if threshold < 0 :
-			print '  (%i, %i)  %f' % (e.GetString(), e.GetOM(), threshold)
+			print '  %s  %f' % (e.GetString(), e.GetOM(), threshold)
