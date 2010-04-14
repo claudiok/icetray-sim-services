f0d945ae3439aad0259fc43f137f1459c99f8fa0
Alex Olivas <olivas@icecube.umd.edu>
2010-04-14 20:45:24 +0000

    updated script
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/sim-services/trunk@63277 16731396-06f5-0310-8873-f7f720988828

--- resources/scripts/test_GCD.py
+++ resources/scripts/test_GCD.py
@@ -9,16 +9,6 @@ from icecube import icetray, dataclasses, dataio, simclasses
 from icecube.BadDomList import bad_dom_list_static
 badOMs = bad_dom_list_static.IC59_static_bad_dom_list()
 
-strings = [ 
-	75, 76, 77, 78, 68, 69, 70, 71, 72, 73,
-	74, 60, 61, 62, 63, 64, 65, 66, 67, 52,
-	53, 54, 55, 56, 57, 58, 59, 44, 45, 46,
-	47, 48, 49, 50, 36, 83, 37, 38, 39, 40,
-	26, 27, 28, 29, 30, 17, 18, 19, 20, 21,
-	10, 11, 12, 13,  2,  3,  4,  5,  6
-	]
-
-
 from optparse import OptionParser
 
 parser = OptionParser()
@@ -46,7 +36,18 @@ dom_geo = geometry.omgeo
 dom_cal = calibration.domCal
 dom_status = status.domStatus
 
+strings_to_check = range(1,87)
+for s in strings_to_check:
+	found = False
+	for e,p in dom_geo:
+		if e.GetString() == s :
+			found = True
+	if not found:
+		print "string %d does not exist in the geometry" % s
+	
+
 for e,p in dom_geo:
+
 	if e in badOMs and e in dom_cal and e in dom_status:
 		print "DOM %s is in the bad DOM list but contains status and calibration records" % str(e)
 	
