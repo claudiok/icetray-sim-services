bd5b522a42bcdce87eb0c69bba80b8c06b9178f8
Don LaDieu <nega@icecube.umd.edu>
2012-03-30 13:18:16 +0000

    python 2.4-ify
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/sim-services/trunk@86704 16731396-06f5-0310-8873-f7f720988828

--- python/sanity_checker/utils/configurations.py
+++ python/sanity_checker/utils/configurations.py
@@ -3,8 +3,8 @@ from icecube import dataclasses as dc
 
 import sys
 
-from ..modules.mctree_checkers import CorsikaTreeSCModule
-from ..modules.launch_checkers import HLCLaunchSCModule
+from icecube.sim_services.sanity_checker.modules.mctree_checkers import CorsikaTreeSCModule
+from icecube.sim_services.sanity_checker.modules.launch_checkers import HLCLaunchSCModule
 
 RunConfigurations = dict()
 RunConfigurations["CORSIKA"] =[ CorsikaTreeSCModule, HLCLaunchSCModule ]
