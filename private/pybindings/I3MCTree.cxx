157a59e7e6f81d9a38399879583be06b921642a4
Alex Olivas <olivas@icecube.umd.edu>
2013-05-31 02:01:48 +0000

    wrap the get method so it's in the I3MCTreeUtils namespace in python-land
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/sim-services/trunk@104864 16731396-06f5-0310-8873-f7f720988828

--- private/pybindings/I3MCTree.cxx
+++ private/pybindings/I3MCTree.cxx
@@ -2,7 +2,17 @@
 #include <simclasses/I3MCPE.h>
 #include <sim-services/I3MCTreeUtils.h>
 
+struct mctreeutils{
+  I3Particle get(const I3MCTree& t, const I3MCPE& pe){
+    return I3MCTreeUtils::Get(t,pe);
+  }
+};
+
+namespace bp = boost::python;
+
 void register_I3MCTree()
 {
-  boost::python::def("get", &I3MCTreeUtils::Get);
+  bp::scope outer =    
+    bp::class_<mctreeutils>("I3MCTreeUtils")
+    .def("get", &mctreeutils::get);
 }
