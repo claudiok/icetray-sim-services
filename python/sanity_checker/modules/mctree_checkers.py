065a4ed4905f5f4c26374f3541abd969f50fd3a2
Alex Olivas <olivas@icecube.umd.edu>
2013-12-23 19:53:13 +0000

    split the tree checkers in three modules 1) general, 2) inice, and 3) icetop.  this may need a rework. single configurable modules is probably better and avoids several passes over the tree, but that'll have to wait for the next version.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/sim-services/trunk@114989 16731396-06f5-0310-8873-f7f720988828

--- python/sanity_checker/modules/mctree_checkers.py
+++ python/sanity_checker/modules/mctree_checkers.py
@@ -23,23 +23,38 @@ class MCTreeSCModule( SCBaseModule ) :
     def __init__(self):
         SCBaseModule.__init__(self)
 
+        self.negLengthParticleCounter = Counter( tolerance = 0 )
+        self.negLengthParticleCounter.failure_msg = "Particles should not have negative lengths."
+        self.register( "negLengthParticleCounter" )
+
+    # returns True if all is well
+    def check( self, frame ):
+        mctree = frame.Get("I3MCTree")
+        for p in mctree :
+            # no particles should have negative lengths
+            self.negLengthParticleCounter.test_condition( (not isnan(p.length)) \
+                                                          and p.length < 0 )
+        # call the base class 'check' method
+        return SCBaseModule.check(self)
+
+class InIceMCTreeSCModule( SCBaseModule ) :
+    def __init__(self):
+        SCBaseModule.__init__(self)
+
         self.nanLengthTrackCounter = Counter( tolerance = 0 )
         self.noInIceMuonsCounter = Counter( tolerance = 100 )
         self.noInIceParticlesCounter = Counter( tolerance = 0 )
         self.unpropagatedTausCounter = Counter( tolerance = 0 )
-        self.negLengthParticleCounter = Counter( tolerance = 0 )
 
         self.nanLengthTrackCounter.failure_msg = "Too many events with NAN length InIce muons."
         self.noInIceMuonsCounter.failure_msg = "Too many events with no InIce muons."
         self.noInIceParticlesCounter.failure_msg = "Too many events with no InIce particles."
         self.unpropagatedTausCounter.failure_msg = "InIce tau not propagated correctly."
-        self.negLengthParticleCounter.failure_msg = "Particles should not have negative lengths."
 
         self.register( "nanLengthTrackCounter" )
         self.register( "noInIceMuonsCounter" )
         self.register( "noInIceParticlesCounter" )
         self.register( "unpropagatedTausCounter" )
-        self.register( "negLengthParticleCounter" )
 
     # returns True if all is well
     def check( self, frame ):
@@ -51,9 +66,6 @@ class MCTreeSCModule( SCBaseModule ) :
 
             if p.location_type == dc.I3Particle.InIce :
                 n_inice_particles += 1
-            # no particles should have negative lengths
-            self.negLengthParticleCounter.test_condition( (not isnan(p.length)) \
-                                                          and p.length < 0 )
 
             if p.location_type == dc.I3Particle.InIce :
                 if ( p.type == dc.I3Particle.MuPlus \
@@ -76,5 +88,14 @@ class MCTreeSCModule( SCBaseModule ) :
         self.noInIceMuonsCounter.test_condition( n_inice_muons == 0 )
         self.noInIceParticlesCounter.test_condition( n_inice_particles == 0 )
 
+        # call the base class 'check' method
+        return SCBaseModule.check(self)
+
+class IceTopMCTreeSCModule( SCBaseModule ) :
+    def __init__(self):
+        SCBaseModule.__init__(self)
+
+    # returns True if all is well
+    def check( self, frame ) : 
         # call the base class 'check' method
         return SCBaseModule.check(self)
--- python/sanity_checker/utils/configurations.py
+++ python/sanity_checker/utils/configurations.py
@@ -4,9 +4,12 @@ from icecube import dataclasses as dc
 import sys
 
 from icecube.sim_services.sanity_checker.modules.mctree_checkers import MCTreeSCModule
+from icecube.sim_services.sanity_checker.modules.mctree_checkers import InIceMCTreeSCModule
+from icecube.sim_services.sanity_checker.modules.mctree_checkers import IceTopMCTreeSCModule
 
 RunConfigurations = dict()
-RunConfigurations["CORSIKA"] = [ MCTreeSCModule ]
-RunConfigurations["NuMu"] = [ MCTreeSCModule ]
-RunConfigurations["NuE"] = [ MCTreeSCModule ]
-RunConfigurations["NuTau"] = [ MCTreeSCModule ]
+RunConfigurations["CORSIKA"] = [ MCTreeSCModule, InIceMCTreeModule ]
+RunConfigurations["NuMu"] = [ MCTreeSCModule, InIceMCTreeModule ]
+RunConfigurations["NuE"] = [ MCTreeSCModule, InIceMCTreeModule ]
+RunConfigurations["NuTau"] = [ MCTreeSCModule, InIceMCTreeModule ]
+RunConfigurations["IceTop"] = [ MCTreeSCModule, IceTopMCTreeModule ]
