325a9b23d0a482ef5b9f2458e1ea4494577f42c6
Alex Olivas <olivas@icecube.umd.edu>
2014-05-19 20:13:03 +0000

    added an event_weight method.  this makes more sense to me than the datalivetime and i think we're going to just switch to this.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/sim-services/trunk@119995 16731396-06f5-0310-8873-f7f720988828

--- python/sanity_checker/histograms/utils.py
+++ python/sanity_checker/histograms/utils.py
@@ -6,7 +6,8 @@ def bins(nbins, min, max) :
     return numpy.arange(min,max,db)
 
 def data_livetime(frame) :
-    
+
+    # does this also work for 5-Comp
     if "CorsikaWeightMap" in frame :
         weightmap = frame["CorsikaWeightMap"]
         return weightmap["TimeScale"]/(weightmap["Weight"] * weightmap["DiplopiaWeight"])
@@ -15,4 +16,30 @@ def data_livetime(frame) :
         weightmap = frame["I3MCWeightDict"]
         return weightmap["NEvents"]/(weightmap["OneWeight"] * frame["DataLiveTime"].value)
 
-    return 1.*I3Units.second
+    if "DataLiveTime" in frame :
+        return frame["DataLiveTime"].value
+
+    return 1.
+
+# Units of s^-1
+def event_weight(frame) :
+
+    if "GaisserH3aWeight" in frame :
+        return frame["GaisserH3aWeight"].value
+
+    # does this also work for 5-Comp
+    if "CorsikaWeightMap" in frame :
+        weightmap = frame["CorsikaWeightMap"]
+        timescale = weightmap["TimeScale"] / I3Units.second
+        return weightmap["Weight"] * weightmap["DiplopiaWeight"] / timescale
+    
+    if "I3MCWeightDict" in frame : 
+        weightmap = frame["I3MCWeightDict"]
+        datalivetime = frame["DataLiveTime"].value / I3Units.second
+        return datalivetime * weightmap["OneWeight"] / weightmap["NEvents"]
+
+    if "DataLiveTime" in frame :
+        datalivetime = frame["DataLiveTime"].value / I3Units.second
+        return 1./ datalivetime
+
+    return 1.
