09dbb78d6b87e22ab17b051aca79b9991a45c820
Alex Olivas <olivas@icecube.umd.edu>
2014-06-11 19:58:38 +0000

    added support for Hoerandel5 and Polygonato
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/sim-services/trunk@120589 16731396-06f5-0310-8873-f7f720988828

--- python/sanity_checker/histograms/utils.py
+++ python/sanity_checker/histograms/utils.py
@@ -11,10 +11,24 @@ def event_weight(frame) :
     if "GaisserH3aWeight" in frame :
         return frame["GaisserH3aWeight"].value
 
+    # This gives the best results, but I'm still off by factors of 2-3
+    # depending on the channel.
+    if "Hoerandel5" in frame :
+        timescale = 1.
+        if "CorsikaWeightMap" in frame :
+            weightmap = frame["CorsikaWeightMap"]
+            timescale = weightmap["TimeScale"]
+        return frame["Hoerandel5"].value
+
+    # This doesn't work for 5-component
+    # I'm off by several orders of magnitude
     if "CorsikaWeightMap" in frame :
         weightmap = frame["CorsikaWeightMap"]
-        timescale = weightmap["TimeScale"] / I3Units.second
-        return weightmap["Weight"] * weightmap["DiplopiaWeight"] / timescale
+        timescale = weightmap["TimeScale"]
+        if "Polygonato" in weightmap :
+            return weightmap["Weight"] * weightmap["DiplopiaWeight"] * weightmap["Polygonato"] / timescale
+        else :
+            return weightmap["Weight"] * weightmap["DiplopiaWeight"] / timescale
     
     if "I3MCWeightDict" in frame : 
         weightmap = frame["I3MCWeightDict"]
