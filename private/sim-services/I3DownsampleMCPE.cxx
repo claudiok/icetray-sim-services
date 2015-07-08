9b25b7661594271b361ca7f153e8a6091875a27a
Alex Olivas <olivas@icecube.umd.edu>
2015-07-08 16:40:26 +0000

    added test for I3DownsampleMCPE.cxx
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/sim-services/trunk@134892 16731396-06f5-0310-8873-f7f720988828

--- private/sim-services/I3DownsampleMCPE.cxx
+++ private/sim-services/I3DownsampleMCPE.cxx
@@ -28,15 +28,14 @@
 /**
  * Randomly downsample MCPEs from one collection to another
  */
-class I3DownsampleMCPE : public I3ConditionalModule
-{
+class I3DownsampleMCPE : public I3ConditionalModule{
 public:
-    I3DownsampleMCPE(const I3Context& ctx);
-    virtual ~I3DownsampleMCPE();
-
-    virtual void Configure();
-    virtual void DAQ(I3FramePtr frame);
-    virtual void Finish();
+  I3DownsampleMCPE(const I3Context& ctx);
+  ~I3DownsampleMCPE(){};
+  
+  void Configure();
+  void DAQ(I3FramePtr frame);
+  void Finish(){};
 
 private:
   std::string inputName_;
@@ -44,55 +43,48 @@ private:
   double      sampleFrac_;
   I3RandomServicePtr rng_;
 
-
-private:
-    SET_LOGGER("I3DownsampleMCPE");
+  SET_LOGGER("I3DownsampleMCPE");
 };
 
 
 I3DownsampleMCPE::I3DownsampleMCPE(const I3Context& ctx) :
-I3ConditionalModule(ctx),
-outputName_("DownsampledMCPEs")
+  I3ConditionalModule(ctx),
+  inputName_("I3MCPESeriesMap"),
+  outputName_("DownsampledMCPEs"),
+  sampleFrac_(0.95)
 {
-    AddParameter("InputName",
-        "Name of the MCPE set to read in and downsample"
-        "InputNameDefault"); 
+  AddParameter("InputName",
+               "Name of the MCPE set to read in and downsample",
+               inputName_);
 
-    AddParameter("OutputName",
-	"Name of the output MCPE series",
-        "OutputNameDefault"); 
+  AddParameter("OutputName",
+               "Name of the output MCPE series",
+               outputName_);
 
-    AddParameter("SampleFrac",
-	"Fraction of MCPEs to accept",
-        0.95); 
+  AddParameter("SampleFrac",
+               "Fraction of MCPEs to accept",
+               0.95);
 
-    AddParameter("RandomService",
-            "A random number generating service (derived from I3RandomService)."
-		 " The default is to get it from the context.",
-		 rng_);
+  AddParameter("RandomService",
+               "A random number generating service (derived from I3RandomService)."
+               " The default is to get it from the context.",
+               rng_);
 
-    AddOutBox("OutBox");
+  AddOutBox("OutBox");
 }
-
-I3DownsampleMCPE::~I3DownsampleMCPE()
-{
-
-}
-
 
 void I3DownsampleMCPE::Configure()
 {
-    GetParameter("InputName", inputName_); 
-    GetParameter("OutputName", outputName_); 
-    GetParameter("SampleFrac", sampleFrac_); 
-    GetParameter("RandomService", rng_);
-    if (!rng_) rng_ = context_.Get<I3RandomServicePtr>();
-    if (!rng_)
-      log_fatal("No random service available. Please add one.");
-    
-    if((sampleFrac_<0)||(sampleFrac_>1))
-      log_fatal("Sample fraction is not a number between 0 and 1.");
-	
+  GetParameter("InputName", inputName_);
+  GetParameter("OutputName", outputName_);
+  GetParameter("SampleFrac", sampleFrac_);
+  GetParameter("RandomService", rng_);
+  if (!rng_) rng_ = context_.Get<I3RandomServicePtr>();
+  if (!rng_)
+    log_fatal("No random service available. Please add one.");
+
+  if((sampleFrac_<0)||(sampleFrac_>1))
+    log_fatal("Sample fraction is not a number between 0 and 1.");
 }
 
 void I3DownsampleMCPE::DAQ(I3FramePtr frame)
@@ -100,50 +92,37 @@ void I3DownsampleMCPE::DAQ(I3FramePtr frame)
 
     I3MCPESeriesMapPtr output(new I3MCPESeriesMap);
     I3MCPESeriesMapConstPtr input = frame->Get<I3MCPESeriesMapConstPtr>(inputName_);
-    
-    if(!input)
-      {
-	log_warn("Frame is missing input MCPE series. cannot downsample");
-      }
+
+    if(!input){
+      log_warn("Frame is missing input MCPE series. cannot downsample");
+    }
+
     for (I3MCPESeriesMap::const_iterator map_iter = input->begin();
-	 map_iter != input->end(); map_iter++)
-        {
-            const OMKey& omkey = map_iter->first; 
-            const I3MCPESeries &pe_series = map_iter->second;
+         map_iter != input->end(); map_iter++){
 
-            for (I3MCPESeries::const_iterator series_iter = pe_series.begin();
-                 series_iter != pe_series.end(); ++series_iter)
-            {
+        const OMKey& omkey = map_iter->first;
+        const I3MCPESeries &pe_series = map_iter->second;
 
-	      // if the MCPE has only 1 pe (most cases), randomly decide                                                                //  whether to keep it or throw it                                                                           
-
-	      if(series_iter->npe <= 1)
-		{
-		  if( rng_->Uniform(1) < sampleFrac_ )
-		    (*output)[omkey].push_back(*series_iter);
-		}
-
-	      // If the MCPE has multiple pe (rare), draw the number to keep                                             
-	      //  from a binomial distribution.                                                                          
-	      else
-		{
-		  int binomialSample = rng_->Binomial(series_iter->npe, sampleFrac_);
-		  I3MCPE newMCPE = *series_iter;
-		  newMCPE.npe = binomialSample;
-		  (*output)[omkey].push_back(newMCPE);
-		}
+        for (I3MCPESeries::const_iterator series_iter = pe_series.begin();
+             series_iter != pe_series.end(); ++series_iter){
+            // if the MCPE has only 1 pe (most cases), randomly decide
+            //  whether to keep it or throw it
+            if(series_iter->npe <= 1){
+              if( rng_->Uniform(1) < sampleFrac_ )
+                (*output)[omkey].push_back(*series_iter);
+            }else{
+              // If the MCPE has multiple pe (rare), draw the number to keep
+              //  from a binomial distribution.
+              int binomialSample = rng_->Binomial(series_iter->npe, sampleFrac_);
+              I3MCPE newMCPE = *series_iter;
+              newMCPE.npe = binomialSample;
+              (*output)[omkey].push_back(newMCPE);
             }
         }
-    
-    
+    }
+
     frame->Put(outputName_, output);
     PushFrame(frame,"OutBox");
-}      
-
-void I3DownsampleMCPE::Finish()
-{
-
 }
-
 
 I3_MODULE(I3DownsampleMCPE);
