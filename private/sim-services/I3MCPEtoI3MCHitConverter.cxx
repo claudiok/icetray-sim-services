91e119ad11e46f0396d9dcf8e82e86bdb7bfab87
Alex Olivas <olivas@icecube.umd.edu>
2015-07-08 18:59:01 +0000

    added sensible defaults to I3MCPEtoI3MCHitConverter.  i don't see the harm in it.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/sim-services/trunk@134903 16731396-06f5-0310-8873-f7f720988828

--- private/sim-services/I3MCPEtoI3MCHitConverter.cxx
+++ private/sim-services/I3MCPEtoI3MCHitConverter.cxx
@@ -35,97 +35,84 @@
 void pe_to_hit(const I3MCPE&, I3MCHit&);
 
 /**
- * Combines several I3MCPEHitSeriesMaps into one.
+ * Converts an I3MCPESeriesMap to an I3MCHitSeriesMap.
  */
 class I3MCPEtoI3MCHitConverter: public I3ConditionalModule
 {
 public:
-    I3MCPEtoI3MCHitConverter(const I3Context& ctx);
-    virtual ~I3MCPEtoI3MCHitConverter();
+  I3MCPEtoI3MCHitConverter(const I3Context& ctx);
+  ~I3MCPEtoI3MCHitConverter(){};
 
-    virtual void Configure();
-    virtual void DAQ(I3FramePtr frame);
-    virtual void Finish();
-
-private:
-    std::string inputResponse_;
-    std::string outputResponse_;
+  void Configure();
+  void DAQ(I3FramePtr frame);
+  void Finish(){};
 
 private:
-    SET_LOGGER("I3MCPEtoI3MCHitSeriesConverter");
+  std::string inputResponse_;
+  std::string outputResponse_;
+
+private:
+  SET_LOGGER("I3MCPEtoI3MCHitSeriesConverter");
 };
 
 
 I3MCPEtoI3MCHitConverter::I3MCPEtoI3MCHitConverter(const I3Context& ctx) :
 I3ConditionalModule(ctx),
-outputResponse_("I3MCHitSeries")
+inputResponse_("I3MCPESeriesMap"),
+outputResponse_("I3MCHitSeriesMap")
 {
-    AddParameter("InputResponse",
-        "Vector of names of the input response serieses to combine (no DEFAULT)",
-        inputResponse_); 
+  AddParameter("InputResponse",
+               "Vector of names of the input response serieses to combine.",
+               inputResponse_);
 
-    AddParameter("OutputResponse",
-        "Name of the output response series",
-        outputResponse_); 
+  AddParameter("OutputResponse",
+               "Name of the output response series",
+               outputResponse_);
 
-    AddOutBox("OutBox");
+  AddOutBox("OutBox");
 }
-
-I3MCPEtoI3MCHitConverter::~I3MCPEtoI3MCHitConverter()
-{
-
-}
-
 
 void I3MCPEtoI3MCHitConverter::Configure()
 {
-    GetParameter("InputResponse", inputResponse_); 
-    GetParameter("OutputResponse", outputResponse_); 
+  GetParameter("InputResponse", inputResponse_);
+  GetParameter("OutputResponse", outputResponse_);
 }
 
 void I3MCPEtoI3MCHitConverter::DAQ(I3FramePtr frame)
 {
-    I3MCHitSeriesMapPtr output(new I3MCHitSeriesMap);
+  I3MCHitSeriesMapPtr output(new I3MCHitSeriesMap);
 
-    I3MCPESeriesMapConstPtr input = frame->Get<I3MCPESeriesMapConstPtr>(inputResponse_);
+  I3MCPESeriesMapConstPtr input = frame->Get<I3MCPESeriesMapConstPtr>(inputResponse_);
 
-    if(!input)
+  if(!input){
+    log_fatal("Frame is missing an input response.");
+  }
+
+  for (I3MCPESeriesMap::const_iterator map_iter = input->begin();
+       map_iter != input->end(); map_iter++)
     {
-            log_fatal("Frame is missing an input response.");
-    }
+      const OMKey& omkey = map_iter->first;
+      const I3MCPESeries &pe_series = map_iter->second;
 
-    for (I3MCPESeriesMap::const_iterator map_iter = input->begin();
-             map_iter != input->end(); map_iter++)
+      for (I3MCPESeries::const_iterator series_iter = pe_series.begin();
+           series_iter != pe_series.end(); ++series_iter)
         {
-            const OMKey& omkey = map_iter->first; 
-            const I3MCPESeries &pe_series = map_iter->second;
+          // this will insert an empty vector automatically in case there is none
 
-            for (I3MCPESeries::const_iterator series_iter = pe_series.begin();
-                 series_iter != pe_series.end(); ++series_iter)
-            {
-                // this will insert an empty vector automatically in case there is none
-                
-                I3MCHitPtr hit(new I3MCHit());
-                pe_to_hit(*series_iter,*hit);
-                (*output)[omkey].push_back(*hit);
-            }
+          I3MCHitPtr hit(new I3MCHit());
+          pe_to_hit(*series_iter,*hit);
+          (*output)[omkey].push_back(*hit);
+        }
 
     }
 
     frame->Put(outputResponse_, output);
     PushFrame(frame,"OutBox");
-}      
-
-void I3MCPEtoI3MCHitConverter::Finish()
-{
-
 }
-
 
 I3_MODULE(I3MCPEtoI3MCHitConverter);
 
-void pe_to_hit(const I3MCPE& pe, I3MCHit& hit)
-{
+void pe_to_hit(const I3MCPE& pe, I3MCHit& hit){
     hit.SetParticleID(pe.ID.majorID,pe.ID.minorID);
     hit.SetTime(pe.time);
     hit.SetNPE(pe.npe);
