67f871bfc79c42d6398fb3fd12117f990566f0f9
Don LaDieu <nega@icecube.umd.edu>
2015-08-21 20:23:49 +0000

    boost/shared_ptr.hpp (and usings) were removed in r2242/IceTray
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/sim-services/trunk@136718 16731396-06f5-0310-8873-f7f720988828

--- private/pybindings/I3PropagatorService.cxx
+++ private/pybindings/I3PropagatorService.cxx
@@ -38,9 +38,9 @@ void register_I3PropagatorService()
         ;
     }
     
-    bp::implicitly_convertible<shared_ptr<I3PropagatorServiceWrapper>, shared_ptr<const I3PropagatorService> >();
-    bp::implicitly_convertible<shared_ptr<I3PropagatorServiceWrapper>, shared_ptr<I3PropagatorService> >();
-    bp::implicitly_convertible<shared_ptr<I3PropagatorServiceWrapper>, shared_ptr<const I3PropagatorServiceWrapper> >();
+    bp::implicitly_convertible<boost::shared_ptr<I3PropagatorServiceWrapper>, boost::shared_ptr<const I3PropagatorService> >();
+    bp::implicitly_convertible<boost::shared_ptr<I3PropagatorServiceWrapper>, boost::shared_ptr<I3PropagatorService> >();
+    bp::implicitly_convertible<boost::shared_ptr<I3PropagatorServiceWrapper>, boost::shared_ptr<const I3PropagatorServiceWrapper> >();
 
     bp::class_<I3ParticleTypePropagatorServiceMap, I3ParticleTypePropagatorServiceMapPtr>("I3ParticleTypePropagatorServiceMap")
     .def(bp::std_map_indexing_suite<I3ParticleTypePropagatorServiceMap>())
